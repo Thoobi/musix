a8d85bd75393a957eaca674eabd1a6e74f9fe0ef
Thoobi <sydneydaniel76@gmail.com>
2025-04-06 17:31:42 +0100

    feat: adds the sidebar again

--- src/App.jsx
+++ src/App.jsx
@@ -2,25 +2,24 @@ import React, { useContext } from "react";
 import Sidebar from "./components/Sidebar";
 import Player from "./components/Player";
 import Display from "./components/Display";
-import { BrowserRouter as Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import Search from "./components/Search";
-
 import { PlayerContext } from "./context/PlayerContext";
 
 const App = () => {
   const { audioRef, track } = useContext(PlayerContext);
+
   return (
     <div className="h-screen bg-black">
       <div className="h-[90%] flex">
         <Sidebar />
-        <Display />
+        <Routes>
+          <Route path="/" element={<Display />} />
+          <Route path="/search" element={<Search />} />
+        </Routes>
       </div>
-      <audio ref={audioRef} src={track.file} preload="auto"></audio>
+      <audio ref={audioRef} src={track?.file} preload="auto" />
       <Player />
-
-      <Routes>
-        <Route path="/search" element={<Search />} />
-      </Routes>
     </div>
   );
 };
